7331205e5772ad0708a0e0072368148b54e009cf
Nyabayo <ernestosindo9@gmail.com>
2025-03-12 12:00:17 +0300

    css_styling_react

---
+++ css-styles/src/App.jsx
@@ -0,0 +1,22 @@
+
+//HOW TO STYLE REACT COMPONENTS WITH CSS
+//(not including external frameworks or preprocessrs)
+//1. EXTERNAL
+//2. MODULES
+//3. INLINE
+
+
+
+import React from "react";
+import Button from "./Button/Button.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Button />
+    </>
+  )
+}
+
+export default App
