82c9236413420633074cbd76201d731d0dfc2dc3
Nyabayo <ernestosindo9@gmail.com>
2025-03-14 19:37:25 +0300

    render lists

--- react_tutorial/css-styles/src/Button/Button.jsx
+++ react_tutorial/css-styles/src/Button/Button.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import styles from './Button.module.css'
 
 function Button(){
-    return(<button className="button">Click Me</button>)
+    return(<button className={styles.button}>Click Me</button>)
+
 }
 
 export default Button
